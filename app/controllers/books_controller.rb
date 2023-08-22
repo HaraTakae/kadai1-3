8a8d402d7a8581f5f0abca0732b2bfcf97938988
HaraTakae <140220718+HaraTakae@users.noreply.github.com>
2023-08-22 08:00:53 +0000

    Add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,22 +7,29 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.all
+    @books = Book.new
     @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
     @user = current_user
+    @users = @book.user
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def create
-    book = Book.find(params[:user_id])
-    book.save
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @user = current_user
+    if @book.save
      redirect_to book_path(@book.id)
+    else
+     render :index
+    end
   end
 
   def update
@@ -31,18 +38,18 @@ class BooksController < ApplicationController
       redirect_to books_path(@book.id)
     end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path(@book.id)
   end
-  
-  
+
+
   private
 
   def book_params
-    params.require(:book).permit(:image, :Title, :Opinion)
+    params.require(:book).permit(:title, :opinion)
   end
 
   def is_matching_login_user
