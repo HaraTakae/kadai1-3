7894c236b0c4fe5bc2dfb2c2f1bbb384747b6efe
HaraTakae <140220718+HaraTakae@users.noreply.github.com>
2023-08-23 04:32:01 +0000

    Add

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   validates :introduction, presence: false, length: { maximum: 50 } # 自己紹介の最高文字数は50文字
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
      profile_image.variant(resize_to_limit: [width, height]).processed
   end
