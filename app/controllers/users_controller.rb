042ddf89b0183826c98723366c3c8ced4b1186af
HaraTakae <140220718+HaraTakae@users.noreply.github.com>
2023-08-24 08:00:23 +0000

    Add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,18 +23,17 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
     redirect_to user_path(@user.id)
+    else
+      render :edit
     end
   end
 
-  # introductionを入れたい
-  def user_params_update
-    params.require(:user).permit(:image, :name, :introduction)
-  end
+
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :image, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
   def is_matching_login_user
